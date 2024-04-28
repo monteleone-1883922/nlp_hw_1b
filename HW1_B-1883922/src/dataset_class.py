9402a48c7310641155a58a38793b21b7d3e9ee76
alex <monteleone.1883922@studenti.uniroma1.it>
2024-04-28 18:19:34 +0200

    refactoring code to deliver

---
+++ HW1_B-1883922/src/dataset_class.py
@@ -0,0 +1,109 @@
+import fasttext.util
+import shutil
+import os
+from torch.utils.data import Dataset, DataLoader
+import nltk
+import json
+import torch
+from torch.nn.utils.rnn import pad_sequence
+import plotly.express as px
+
+nltk.download('punkt')
+
+
+class HaSpeeDe_Dataset(Dataset):
+
+    def __init__(self, data_path: str, data: list[tuple[list, int]] = None, use_embeddings: bool = False,
+                 stopwords_file_path: str = "", device="cpu", encoder=None, direct_embeddings=True) -> None:
+        self.device = device
+        if not direct_embeddings:
+            use_encoder = True
+            if encoder is None:
+                encoder = {}
+                use_encoder = False
+                j = 0
+                new_embeddings = {}
+        if data is not None:
+            self.data = data
+        else:
+            stopwords = []
+            if stopwords_file_path != "":
+                with open(stopwords_file_path, 'r', encoding="UTF8") as f:
+                    stopwords = f.readlines()  # controllare carattere di andare a capo
+            if use_embeddings:
+                fasttext.util.download_model('it', if_exists='ignore')
+                if os.path.exists('cc.it.300.bin.gz'):
+                    os.remove('cc.it.300.bin.gz')
+                shutil.move('cc.it.300.bin', 'data/cc.it.300.bin')
+                embeddings = fasttext.load_model('data/cc.it.300.bin')
+            self.data = []
+            with open(data_path, 'r', encoding="UTF8") as f:
+                for line in f:
+                    item = json.loads(line)
+                    sentence = nltk.word_tokenize(item['text'], language='italian')
+                    filtered_sentence = []
+                    i = 0
+                    while i < len(sentence):
+                        word = sentence[i]
+                        if (word == "#" or word == "@") and i + 1 < len(sentence):
+                            word = word + sentence[i + 1]
+                            i += 1
+                        if stopwords_file_path == "" or word not in stopwords:
+                            filtered_sentence.append(word)
+                        i += 1
+
+                    if use_embeddings:
+                        embedded_sentence = []
+                        for word in sentence:
+                            if word not in encoder and not use_encoder and not direct_embeddings:
+                                encoder[word] = j
+                                new_embeddings[j] = embeddings.get_word_vector(word)
+                                embedded_sentence.append(j)
+                                j += 1
+                            elif word not in encoder and not direct_embeddings:
+                                embedded_sentence.append(encoder["<UNK>"])
+                            else:
+                                embedded_sentence.append(encoder[word])
+                        if len(embedded_sentence) == 0:
+                            print("error empty sentence")
+                        sentence = embedded_sentence
+                    self.data.append((sentence, item['label']))
+            if not use_encoder and not direct_embeddings:
+                encoder["<UNK>"] = j
+                self.encoding = encoder
+                new_embeddings[j] = embeddings.get_word_vector("<UNK>")
+                self.embeddings = new_embeddings
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __getitem__(self, idx: int) -> tuple[list, int]:
+        return self.data[idx]
+
+    def split(self, prc: float) -> list[tuple[list, int]]:
+        validation_size = int(prc * len(self.data))
+        train_size = len(self.data) - validation_size
+        validation_data, self.data = torch.utils.data.random_split(self.data, [validation_size, train_size])
+        return validation_data
+
+    def collate(self, batch: list[tuple[list, int]]) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        texts, labels = zip(*batch)
+        lens = [len(text) for text in texts]
+        texts = pad_sequence([torch.tensor(text) for text in texts], batch_first=True)
+        return texts.to(self.device), torch.tensor(labels, dtype=torch.float).to(self.device), torch.tensor(lens).to(
+            self.device)
+
+    def get_dataloader(self, batch_size: int, shuffle: bool) -> DataLoader:
+        return DataLoader(self, batch_size=batch_size, shuffle=shuffle, collate_fn=self.collate)
+
+    def print_data_analysis(self):
+        y = [0, 0]
+        for el in self.data:
+            if el[1] == 1:
+                y[1] += 1
+            else:
+                y[0] += 1
+        self.neutral_count = y[0]
+        self.hateful_count = y[1]
+        fig = px.bar(x=["neutrale", "odio"], y=y)
+        fig.show()
---
+++ HW1_B-1883922/src/trainer.py
@@ -0,0 +1,122 @@
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
+import torch
+import sys
+
+
+# Function to print a progress bar
+
+
+class Trainer():
+
+    def __init__(self, model, train_dataloader, validation_dataloader, optimizer, loss_function, device,
+                 test_dataloader=None):
+        self.model = model.to(device)
+        self.train_dataloader = train_dataloader
+        self.validation_dataloader = validation_dataloader
+        self.optimizer = optimizer
+        self.loss_function = loss_function
+        self.device = device
+        self.test_dataloader = test_dataloader
+
+    @staticmethod
+    def evaluation_parameters(y_true, y_pred):
+        cm = confusion_matrix(y_true, y_pred).ravel()
+        precision = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        accuracy = accuracy_score(y_true, y_pred)
+        return cm, precision, recall, f1, accuracy
+
+    @staticmethod
+    def print_progress_bar(percentuale: float, lunghezza_barra: int = 30, text: str = "") -> None:
+        blocchi_compilati = int(lunghezza_barra * percentuale)
+        barra = "[" + "=" * (blocchi_compilati - 1) + ">" + " " * (lunghezza_barra - blocchi_compilati) + "]"
+        sys.stdout.write(f"\r{barra} {percentuale * 100:.2f}% complete " + text)
+        sys.stdout.flush()
+
+    def train(self, epochs: int, use_wandb: bool = False, config: dict = {}, name: str = "", target_f1: float = 0.0,
+              validate: bool = True):
+        best_model = None
+        save = False
+        if use_wandb:
+            pass
+            # wandb.init(
+            #     # Set the project where this run will be logged
+            #     project="nlp-hw-1b",
+            #     name=name,
+            #     # Track hyperparameters and run metadata
+            #     config=config
+            # )
+        for epoch in range(epochs):
+            if epoch == 3:
+                self.model.freeze_embeddings(False)
+            self.model.train()  # Set the model to training mode
+            total_loss = 0
+            for i, batch in enumerate(self.train_dataloader):
+                self.print_progress_bar(i / len(self.train_dataloader), text=f"| training epoch {epoch}")
+                # Get the inputs and targets from the batch
+                inputs, targets, lens = batch
+
+                # Zero the gradients
+                self.optimizer.zero_grad()
+                # Forward pass
+                outputs = self.model((inputs, lens))
+                # Compute loss
+                loss = self.loss_function(outputs, targets)
+                # Backward pass and optimize
+                loss.backward()
+                self.optimizer.step()
+                # Accumulate the total loss
+                total_loss += loss.item()
+
+            # Print the average loss for this epoch
+            if validate:
+                validation_loss, precision, recall, f1, accuracy = self.validate(use_wandb)
+                if f1 > target_f1:
+                    best_model = self.model.state_dict()
+                    target_f1 = f1
+                    save = True
+                if use_wandb:
+                    pass
+                    # wandb.log({"validation_loss": validation_loss,
+                    #       "precision": precision,
+                    #       "recall": recall,
+                    #       "f1": f1,
+                    #       "accuracy": accuracy,
+                    #       "train_loss": total_loss / len(self.train_dataloader)})
+        if save:
+            torch.save(best_model, name + f'-{target_f1}.pth')
+        if use_wandb:
+            pass
+            # wandb.finish()
+
+    def validate(self, use_wandb: bool = False, test=False):
+        dataloader = self.test_dataloader if test else self.validation_dataloader
+        if dataloader is None:
+            print("empty dataloader!")
+            exit(1)
+        self.model.eval()  # Set the model to evaluation mode
+        total_loss = 0
+        all_predictions = torch.tensor([])
+        all_targets = torch.tensor([])
+        with torch.no_grad():  # Do not calculate gradients
+            for i, batch in enumerate(self.validation_dataloader):
+                self.print_progress_bar(i / len(dataloader), text="| validation")
+                # Get the inputs and targets from the batch
+                inputs, targets, lens = batch
+
+                # Forward pass
+                outputs = self.model((inputs, lens))
+                # Compute loss
+                loss = self.loss_function(outputs, targets)
+                # Accumulate the total loss
+                total_loss += loss.item()
+                # Store predictions and targets
+                all_predictions = torch.cat((all_predictions, outputs.squeeze().round().cpu()))
+                all_targets = torch.cat((all_targets, targets.cpu()))
+        validation_loss = total_loss / len(self.validation_dataloader)
+        precision = precision_score(all_targets, all_predictions)
+        recall = recall_score(all_targets, all_predictions)
+        f1 = f1_score(all_targets, all_predictions)
+        accuracy = accuracy_score(all_targets, all_predictions)
+        return validation_loss, precision, recall, f1, accuracy
